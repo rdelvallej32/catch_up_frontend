590bba90c1a692b9b25fcda42add0c5b1a1013d0
rdelvallej32 <rdelvallej@gmail.com>
2016-04-28 18:12:54 -0400

    Add auth folder for authentication js files

---
+++ assets/scripts/app_data.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const app = {
+  api: 'http://tic-tac-toe.wdibos.com',
+};
+
+
+module.exports = app;
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,12 @@
-'use strict';
-
-// user require with a reference to bundle the file and use it in this file
-// var example = require('./example');
-
-// use require without a reference to ensure a file is bundled
-require('./example');
+// 'use strict';
+//
+// // user require with a reference to bundle the file and use it in this file
+// // var example = require('./example');
+// const authEvents = require('./auth/events.js');
+// //
+// // // use require without a reference to ensure a file is bundled
+// // require('./example');
+//
+// $(() => {
+//
+// });
