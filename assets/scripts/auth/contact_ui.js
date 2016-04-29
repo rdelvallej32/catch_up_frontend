c0b5550ecbc7a903c21ffdf912daab930b146f93
rdelvallej32 <rdelvallej@gmail.com>
2016-04-29 11:34:06 -0400

    add contact ui file

---
+++ assets/scripts/auth/contact_ui.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const app = require('./app_data.js');
+
+
+const success = (data) => {
+  console.log(data);
+  console.log("Buenisimo");
+};
+
+const failure = (error) => {
+  console.error(error);
+};
+
+module.exports = {
+  failure,
+  success,
+
+};
